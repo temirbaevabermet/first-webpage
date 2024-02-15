5b5f38b4cc9dae1f8b2d8f8dd50a28c30540cbd9
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-02-15 21:12:20 +0600

    init commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello World!";
+
+let myVariable = 'mmm';
+alert(myVariable);
+const imgElement = document.querySelector("img");
+const img1URL = "images/alatoo-logo.png";
+const img2URL = "images/forest.webp";
+function changePhoto(event){
+    let current = imgElement.getAttribute("src");
+    if(current == img1URL){
+        imgElement.setAttribute("src", img2URL);
+    }
+    else{
+        imgElement.setAttribute("src",img1URL);
+    }
+}
+imgElement.onclick = changePhoto;
